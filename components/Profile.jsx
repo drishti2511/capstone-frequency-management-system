ca424f4d11e87cdd4e365c21b44ee87c3f944389
aena ghai <aenaghai5@gmail.com>
2023-11-13 23:44:57 +0530

    Profile Page UI

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -10,9 +10,10 @@ import { styled } from '@mui/system';
 
 const Profile = () => {
     const [userData, setUserData] = useState(null);
-    const [latitude1,setLatitude1] = useState('');
-    const [longitude1,setLongitude1] = useState('');
+    const [latitude1, setLatitude1] = useState('');
+    const [longitude1, setLongitude1] = useState('');
     const { data: session } = useSession();
+
     useEffect(() => {
         const email = localStorage.getItem('email');
         // const email = session.user.email;
@@ -28,13 +29,13 @@ const Profile = () => {
 
             const position = await new Promise((resolve, reject) => {
                 navigator.geolocation.getCurrentPosition(resolve, reject);
-              });
-              const { latitude, longitude } = position.coords;
-              setLatitude1(latitude);
-              setLongitude1(longitude);
-              console.log('latitude :',latitude);
-              console.log('longitude :',longitude);
-        
+            });
+            const { latitude, longitude } = position.coords;
+            setLatitude1(latitude);
+            setLongitude1(longitude);
+            console.log('latitude :', latitude);
+            console.log('longitude :', longitude);
+
 
             try {
                 console.log('inside-loaduserprof-temp');
@@ -69,141 +70,205 @@ const Profile = () => {
     return (
         <Container maxWidth="md">
             <Paper elevation={3} style={{ padding: 20, marginTop: 40 }}>
-                <Typography variant="h5">User Profile</Typography>
+                <Typography variant="h5" gutterBottom>
+                    User Profile
+                </Typography>
                 <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <TextField
-                        label="Name"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        color="secondary"
-                        disabled
-                    />
-                    <TextField
-                        label="Email"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        disabled
+                    <Grid item xs={4} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '14.5px'
+                            }}
+                        >
+                            Name
+                        </Typography>
 
-                    />
-                    <TextField
-                        label="Contact Number"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        disabled
-                    />
-                    <TextField
-                        label="Designation"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        disabled
-                    />
-                    <TextField
-                        label="Radio Details"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        disabled
-                    />
-                    <TextField
-                        label="Radio Set Details"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        disabled
-                    />
-                    <TextField
-                        label="Latitude"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        disabled
-                    />
-                    <TextField
-                        label="Longitude"
-                        variant="filled"
-                        fullWidth
-                        margin="normal"
-                        disabled
-                    />
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '11px'
+                            }}
+                        >
+                            Email
+                        </Typography>
+
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '12px'
+                            }}
+                        >
+                            Contact Number
+                        </Typography>
+
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '10px'
+                            }}
+                        >
+                            Designation
+                        </Typography>
+
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '11px'
+                            }}
+                        >
+                            Radio Details
+                        </Typography>
+
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '12px'
+                            }}
+                        >
+                            Radio Set Details
+                        </Typography>
+
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '11px'
+                            }}
+                        >
+                            Latitude
+                        </Typography>
+
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                backgroundColor: '#1e90ff',
+                                color: 'white',
+                                padding: '17px',
+                                borderRadius: '8px',
+                                textAlign: 'center',
+                                fontWeight: 'bold',
+                                margin: '11px'
+                            }}
+                        >
+                            Longitude
+                        </Typography>
+
                     </Grid>
                     <Grid item xs={6}>
-                    {userData && (
-                        <div>
-                            <TextField
-                                // label="Name"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userData.name}
-                                
-                            />
-                            <TextField
-                                // label="Email"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userData.email}
+                        {userData && (
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <TextField
+                                    // label="Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={userData.name}
+
+                                />
+                                <TextField
+                                    // label="Email"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={userData.email}
                                 // disabled
-                            />
-                            <TextField
-                                // label="Contact Number"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userData.contactNumber}
+                                />
+                                <TextField
+                                    // label="Contact Number"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={userData.contactNumber}
                                 // disabled
-                            />
-                            <TextField
-                                // label="Designation"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userData.designation}
+                                />
+                                <TextField
+                                    // label="Designation"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={userData.designation}
                                 // disabled
-                            />
-                            <TextField
-                                // label="Radio Details"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userData.radioDetails}
+                                />
+                                <TextField
+                                    // label="Radio Details"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={userData.radioDetails}
                                 // disabled
-                            />
-                            <TextField
-                                // label="Radio Set Details"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userData.radioSetDetails}
+                                />
+                                <TextField
+                                    // label="Radio Set Details"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={userData.radioSetDetails}
                                 // disabled
-                            />
-                            <TextField
-                                // label="Location"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={latitude1}
-                                style={{color:'red'}}
+                                />
+                                <TextField
+                                    // label="Location"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={latitude1}
+                                    style={{ color: 'red' }}
                                 // disabled
-                            />
-                             <TextField
-                                // label="Location"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={longitude1}
-                                style={{color:'red'}}
+                                />
+                                <TextField
+                                    // label="Location"
+                                    variant="outlined"
+                                    fullWidth
+                                    margin="normal"
+                                    value={longitude1}
+                                    style={{ color: 'red' }}
                                 // disabled
-                            />
-                        </div>
-                    )}
+                                />
+                            </div>
+                        )}
                     </Grid>
-                    </Grid>
+                </Grid>
             </Paper>
         </Container>
     );
